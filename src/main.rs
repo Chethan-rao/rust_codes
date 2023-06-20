3b51e986ca19fffc0594311b1883513652e88424
Chethan <chethan.rao@juspay.in>
2023-06-20 22:55:51 +0530

    add CI workflows

--- src/main.rs
+++ src/main.rs
@@ -1125,4 +1125,18 @@
 //     }
 // }
 
-fn main() {}
+/* Make it work with least amount of changes*/
+fn main() {
+    let color = String::from("green");
+
+    let print = move || println!("`color`: {}", color);
+
+    print();
+    print();
+
+    // `color` can be borrowed immutably again, because the closure only holds
+    // an immutable reference to `color`.
+    let _reborrow = &color;
+
+    println!("{}", color);
+}
